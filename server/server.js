67d312687a50749a3daf1229026a1d3f7e261263
ckp8 <charanjit127@hotmail.com>
2020-11-16 15:53:59 +0000

    files updated

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-/*
-
 //creating server
 const express = require ('express')
 const server = express();
@@ -10,23 +8,35 @@ server.use(bodyParser.json())
 
 //To read data from the post database file asynchronously 
 const fs = require('fs')
-const postData =fs.readFile('./database.json', 'utf8', (err, data) => {
+ fs.readFile('./database.json', 'utf8', (err, data) => {
+
+    if (err) {
+        console.log(`Error reading file from disk: ${err}`);
+    } else {
+        const databases = JSON.parse(data);
+        databases.push({
+            name: 'cheese4',
+            type: 'halumi'
+        });
+
+        fs.writeFile('./database.json', JSON.stringify(databases, null, 4), (err) => {
+            if (err) {
+                console.log(`Error writing file: ${err}`);
+            }
+        });
+    }
+
+});
+
+//const postData =fs.readFile('./database.json', 'utf8', (err, data) => {
     //if (err) {
        // console.log(`Error reading file from disk: ${err}`);
-    const database = JSON.parse(data) // parse JSON string into JSON object
-    database.push ({
-        name: 'cheese4',
-        type: 'halumi'
-    });
+    //const database = JSON.parse(data) // parse JSON string into JSON object
+   // database.push ({
+        //name: 'cheese4',
+        ////type: 'halumi'
+   // });
 
-    const updatedFile = fs.writeFile('./database.json', JSON.stringify(databases, null, 4), (err) => {
-        if (err) {
-            console.log(`Error writing file: ${err}`);
-        }
-    });
-}
-
-});   
 
 
 
@@ -36,7 +46,7 @@ server.listen(3000, () => console.log("Server being departed!"))
 //checking if server works with dummy results
 const cheese = ['brie', 'mozzarela', 'cheddar']
 server.get('/cheeses', (req, res) => {
-  res.send(updatedDatabase)
+  res.send(cheese)
      });
 
      server.post('/cheeses', (req,res) => {
@@ -48,5 +58,3 @@ server.get('/cheeses', (req, res) => {
 
 
 module.exports = server;
-
-*/
