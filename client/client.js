95596fb4651eab95aadd6ee8e6fcf146694783ce
derejmi <derejemireyigbe@gmail.com>
2020-11-16 15:23:24 +0000

    fix client bugs

--- client/client.js
+++ client/client.js
@@ -1,98 +1,76 @@
-
-/*
-
 //Set up
-const form = document.querySelector(".post-form")
-const spinner = document.querySelector(".spinner")
-const postElement = document.querySelector(".posts")
+const form = document.querySelector(".post-form");
+const spinner = document.querySelector(".spinner");
+const postElement = document.querySelector(".posts");
 
 //On-loading hide spinner
-spinner.style.display = 'none'
+spinner.style.display = "none";
 
+loadPosts();
 //On load - load all the posts
 
-function loadPosts(){
+function loadPosts() {
+  fetch("http://localhost:3000/cheeses")
+    .then((r) => r.json())
+    .then((posts) => {
+      posts.reverse();
+      posts.forEach((post) => {
+        const div = document.createElement("div");
 
-fetch('http://localhost:3000/cheeses')
-   .then(r => r.json())
-    .then(posts => {
-        posts.reverse();
-         posts.forEach((post) => { 
-        const div = document.createElement('div')
-        
         //headers to show usernames
-        const header = document.createElement('h3')
-        header.textContent = post.name
-        
+        const header = document.createElement("h3");
+        header.textContent = post.name;
+
         //post content
-        const contents = document.createElement('p')
-        header.textContent = post.content
-        
+        const contents = document.createElement("p");
+        header.textContent = post.content;
+
         //date
-        const date = document.createElement('small')
-        date.textContent =new Date(post.created)
+        const date = document.createElement("small");
+        date.textContent = new Date(post.created);
 
         //comment button
-        const comment = document.createElement('button')
-        comment.className = "comment"
-        comment.value = "Comment"
+        const comment = document.createElement("button");
+        comment.className = "comment";
+        comment.value = "Comment";
 
-
-        div.appendChild(header)
-        div.appendChild(contents)
-        div.appendChild(date)
-        div.appendChild(button)
-
-
-
-
-    
-    })
-       
-    
-    
-    
-    
-
-
-
-      
+        div.appendChild(header);
+        div.appendChild(contents);
+        div.appendChild(date);
+        div.appendChild(comment);
+      });
+    });
 }
-
-
-
 
 //Form submission logic
 
-form.addEventListener('submit', (event) => {
-event.preventDefault();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-const name = event.target.name.value
-const content = event.target.content.value
+  const name = event.target.name.value;
+  const content = event.target.content.value;
 
-const post ={
-name,
-content
-}
-//(Hides form)
-form.style.display = 'none'
-//(shows spinner)
-spinner.style.display = ''
-const options = {
+  const post = {
+    name,
+    content,
+  };
 
-method: 'POST',
-body: JSON.stringify(post),
-headers: { 
-    'content-type' : 'application/json'
-}
-}
+  //(Hides form)
+  form.style.display = "none";
+  //(shows spinner)
+  spinner.style.display = "";
 
-fetch('http://localhost:3000/cheeses',options 
-)
-.then(r=>r.text())
-.then(message=>{console.log(message)})
+  const options = {
+    method: "POST",
+    body: JSON.stringify(post),
+    headers: {
+      "content-type": "application/json",
+    },
+  };
 
-
-}
-
-*/
+  fetch("http://localhost:3000/cheeses", options)
+    .then((r) => r.text())
+    .then((message) => {
+      console.log(message);
+    });
+});
