a91034d46e311783cd50ba1fde9783185803d7b0
derejmi <derejemireyigbe@gmail.com>
2020-11-17 17:02:21 +0000

    add server emoji logic

--- server/server.js
+++ server/server.js
@@ -80,6 +80,10 @@ server.post("/posts", (req, res) => {
       name: incomingRequest.name.toString(),
       content: incomingRequest.content.toString(),
       date: new Date(),
+      likes: 0,
+      dislikes: 0,
+      laughs: 0,
+      comments: [],
       //id : database.length
     };
 
@@ -115,5 +119,41 @@ server.post("/posts", (req, res) => {
 //   //   cheese.push(newCheese);
 //   res.send("cheese");
 // });
+
+//POST ROUTE - EMOJIS
+
+server.post("/emojis", (req, res) => {
+  const incomingRequest = req.body;
+  const emoji = incomingRequest.emoji;
+  const id = incomingRequest.id;
+
+  fs.readFile("./database.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(`Error reading file from disk: ${err}`);
+    } else {
+      const postsData = JSON.parse(data);
+      if (emoji === "fa-thumbs-up") {
+        postsData[id].likes++;
+      }
+      if (emoji === "fa-thumbs-down") {
+        postsData[id].dislikes++;
+      }
+      if (emoji === "fa-laugh-squint") {
+        postsData[id].laughs++;
+      }
+
+      fs.writeFile(
+        "./database.json",
+        JSON.stringify(postsData, null, 4),
+        (err) => {
+          if (err) {
+            console.log(`Error writing file: ${err}`);
+          }
+        }
+      );
+    }
+    res.send([emoji, id]);
+  });
+});
 
 module.exports = server;
