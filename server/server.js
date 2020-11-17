ab869838345d153e39dc30e4cfd72c52bce1d705
Olivia <olivia.vacaru@gmail.com>
2020-11-17 08:40:35 +0000

    working get route request with updated code

--- server/server.js
+++ server/server.js
@@ -9,61 +9,55 @@ const bodyParser = require('body-parser');
 server.use(bodyParser.json())
 
 //To read data from the post database file asynchronously 
-const fs = require('fs')
-fs.readFile('./database.json', 'utf8', (err, data) => {
+//const fs = require('fs')
+//fs.readFile('./database.json', 'utf8', (err, data) => {
 
-    if (err) {
-        console.log(`Error reading file from disk: ${err}`);
-    } else {
-        const databases = JSON.parse(data);
-        databases.push({
-            name: 'cheese4',
-            type: 'halumi'
-        });
+   // if (err) {
+      //  console.log(`Error reading file from disk: ${err}`);
+   // } else {
+      //  const databases = JSON.parse(data);
+        //databases.push({
+           // name: 'cheese4',
+           // type: 'halumi'
+        //});
 
-        fs.writeFile('./database.json', JSON.stringify(databases, null, 4), (err) => {
-            if (err) {
-                console.log(`Error writing file: ${err}`);
-            }
-        });
-    }
+      // fs.writeFile('./database.json', JSON.stringify(databases, null, 4), (err) => {
+         //  if (err) {
+            //   console.log(`Error writing file: ${err}`);
+            //}
+        //});
+    //}
 
-});
+//});
 
-const updatedData = require('./database.json');
-//const postData =fs.readFile('./database.json', 'utf8', (err, data) => {
-    //if (err) {
-       // console.log(`Error reading file from disk: ${err}`);
-    //const database = JSON.parse(data) // parse JSON string into JSON object
-   // database.push ({
-        //name: 'cheese4',
-        ////type: 'halumi'
-   // });
-
-
+//const updatedData = require('./database.json');
 
 
 //running server
 server.listen(3000, () => console.log("Server being departed!"))
 
+const fs = require('fs')
+server.get("/cheeses",(req,res) => {
+    fs.readFile("./database.json", "utf8", (err,data) => {
+    if (err) {
+console.log(`Error reading file from disk: ${err}`);
+    } else {
+        const posts =JSON.parse(data);
+        res.json(posts);
+    }
+});
+});
+
+
 //checking if server works with dummy results
 //const cheese = ['brie', 'mozzarela', 'cheddar']
-server.get('/cheeses', (req, res) => {
-  res.json(updatedData)
-     });
+//server.get('/cheeses', (req, res) => {
+ // res.json(updatedData)
+   //  });
 
- server.post('/cheeses', (req,res) => {
-    const newCheese = {
-        name: 'cheese5',
-        type: 'gorgonzola'
-    };
-     const newData = updatedData.push(newCheese)
-     res.send(newData)
-})
-     //cheese.push(newCheese);
-    // res.send("cheese");
-    // });
-
+ //server.post('/cheeses', (req,res) => {
+    
+     
 
 module.exports = server;
 
