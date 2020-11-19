04e79904fca3715d0ba0937a5e9dc6454d3b4671
ckp8 <43785332+ckp8@users.noreply.github.com>
2020-11-19 15:22:08 +0000

    Update test.js
    added test <3

--- test/test.js
+++ test/test.js
@@ -1,11 +1,18 @@
 const request = require('supertest');
 const server = require ('../server');
 const expect = require('chai').expect;
-//const describe = require('mocha').describe;
+const chaiHttp = require("chai-http");
+ let chai = require('chai');
+ 
+const { post, response } = require('../server/server');
 
+// const { expect } = chai;
+chai.use(chaiHttp);
 
 describe('API endpoints', () => {
     let api 
+  
+    
     before(() => {
         console.log('setting up')
         api = server.listen(3000, 'localhost');
@@ -16,18 +23,29 @@ describe('API endpoints', () => {
     //         .get('/')
     //         .expect(200, done);
     // });
+    // it("will post", done => {
+    //     chai
+    //       .request(app)
+    //       .post("/posts")
+    //       .send({ num1: 5, num2: 5 })
+    //       .end((err, res) => {
+    //         expect(res).to.have.status(200);
+    //         expect(res.body.status).to.equals("success");
+    //         expect(res.body.result).to.equals(10);
+    //         done();
+    //       });
 
-    it('responds to /cheeses', done => {
-        request(api)
-            .get('/cheeses')
-            .expect(200, done);
-    });
+  
 
     it('404 everything else', done => {
         request(api)
             .get('/apple')
             .expect(404, done);
     });
+    it('should take less than 10000ms', function(done){
+        this.timeout(10000);
+        setTimeout(done, 300);
+      });
 
      after(done => {
         // close the server, then run done
@@ -35,31 +53,62 @@ describe('API endpoints', () => {
         api.close(done)
     })
 
+
+
 })
-// describe('API server', () => {
-//     let api
-//     let testCheese = {
-//         "name": "mozzerela",
+//testing POST methods
+describe ("POST /post methods",() => {
+    it("should get /post ",(done)=>{
+        const incomingRequest = {
+            name: "charanjit",
+            content: "im posting",
+            gif: ""
+
+        };
+        chai.request(server)
+        .post("/posts")
+        .send(incomingRequest)
+        .end(function (err, res) {
+          expect(res).to.have.status(500)
+          done()
+        });
+ 
+
         
-//     }
+        
+        
 
-// it('responds to post /cheeses with status 201', done => {
-//     request(api)
-//         .post('/cheeses')
-//         .send(testCheese)
-//         .expect(201)
-//         .expect({id: 4, ...testCheese}, done)
-// })
-// })
 
-describe('/POST cheeses', () => {
-    it('it should  POST cheese ', (done) => {
-        let cheese = {
-            name: "mozerrela",
-           
-        }
-      chai.request(server)
-          .post('/cheese')
-          .send(cheese)
     })
+    it("should get /comments ",(done)=>{
+        const commente = {
+           comment: "hi omg"
+
+        };
+        chai.request(server)
+        .post("/comments")
+        .send(commente)
+        .end(function (err, res) {
+          expect(res).to.have.status(201)
+          done()
+        });
+
+
+})
+// it("should get /emoji ",(done)=>{
+//     const commente = {
+//        comment: "hi omg"
+
+//     };
+//     chai.request(server)
+//     .post("/emoji")
+//     .send(commente)
+//     .end(function (err, res) {
+//       expect(res).to.have.status(201)
+//       done()
+//     });
+
+
+
+// 
 })
